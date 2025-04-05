542b91c08906d54c6455a8ed0c99cc67e410aba2
unknown <georgesparks673@gmail.com>
2025-04-05 11:18:49 -0500

    added the majority of polar and spherical coordinates along with their tests. Polar and spherical coordinate set setters still needed

--- src/PanCAD/geometry/point.py
+++ src/PanCAD/geometry/point.py
@@ -2,65 +2,208 @@
 graphics, and other geometry use cases.
 """
 
+import math
+
 import numpy as np
+
+
 
 class Point:
     
-    def __init__(self, position: tuple[float, float, float] = (None, None), *,
+    def __init__(self, position: tuple[float, float, float] = (None, None, None), *,
                  uid: str = None, unit: str = None):
         
-        if uid:
-            self.uid = uid
-        
-        if position:
-            self.position = position
-        
-        if unit:
-            self.unit = unit
+        self.uid = uid if uid else None
+        self.position = position if position else (None, None, None)
+        self.unit = unit if unit else None
     
     # Getters #
     @property
-    def position(self) -> tuple[float, float, float]:
+    def uid(self) -> str:
+        """Returns the unique id of the point"""
+        return self._uid
+    
+    # Cartesian Coordinates #
+    @property
+    def position(self) -> tuple[float, float] | tuple[float, float, float]:
+        """Returns the cartesian coordinates of the point"""
         return self._position
     
     @property
-    def uid(self) -> str:
-        return self._uid
-    
-    @property
     def x(self) -> float:
-        return self._x
+        """Returns the cartesian x-coordinate of the point"""
+        return self.position[0]
     
     @property
     def y(self) -> float:
-        return self._y
+        """Returns the cartesian y-coordinate of the point"""
+        return self.position[1]
     
     @property
-    def z(self):
-        return self._z
+    def z(self) -> float:
+        """Returns the cartesian z-coordinate of the point. Will error if 
+        the point is 2D."""
+        return self.position[2]
+    
+    # Polar/Spherical Coordinates
+    @property
+    def polar(self) -> tuple[float, float]:
+        """Returns the polar coordinate (r, phi) of the point. Azimuth angle 
+        is in radians. Will error if the point is 3D."""
+        if len(self.position) == 2:
+            return (self.r, self.phi)
+        else:
+            raise ValueError("Point must be 2D to return a polar coordinate, "
+                             + "use spherical() for 3D points")
+    
+    @property
+    def spherical(self) -> tuple[float, float, float]:
+        """Returns the polar coordinate (r, phi, theta) of the point. Azimuth 
+        and inclination angles are in radians. Will error if the point is 
+        2D."""
+        return (self.r, self.phi, self.theta)
+    
+    @property
+    def r(self) -> float:
+        """Returns the polar/spherical radial coordinate of the point"""
+        if len(self.position) == 2:
+            return math.hypot(self.x, self.y)
+        else:
+            return math.hypot(self.x, self.y, self.z)
+    
+    @property
+    def phi(self) -> float:
+        """Returns the polar/spherical azimuth coordinate of the point in 
+        radians"""
+        if self.x == 0 and self.y == 0:
+            return math.nan
+        else:
+            return math.atan2(self.y, self.x)
+    
+    @property
+    def theta(self) -> float:
+        """Returns the spherical inclination coordinate of the point in 
+        radians"""
+        if self.z == 0 and math.hypot(self.x, self.y) != 0:
+            return math.pi/2
+        elif self.x == 0 and self.y == 0 and self.z == 0:
+            return math.nan
+        elif self.z > 0:
+            return math.atan(math.hypot(self.x, self.y)/self.z)
+        elif self.z < 0:
+            return math.pi + math.atan(math.hypot(self.x, self.y)/self.z)
+        else:
+            raise ValueError(f"Unhandled exception, position: {self.position}")
     
     # Setters #
     @position.setter
     def position(self, position: tuple[float, float, float]):
-        self._position = position
+        if len(position) == 2 or len(position) == 3:
+            self._position = position
+        else:
+            raise ValueError(f"Given position {position} needs 2 or 3 elements")
     
     @uid.setter
-    def uid(self, uid: str):
+    def uid(self, uid: str) -> None:
         self._uid = uid
     
     @x.setter
-    def x(self, value: float):
-        self.position[0] = value
+    def x(self, value: float) -> None:
+        if len(self.position) == 2:
+            self.position = (value, self.position[1])
+        else:
+            self.position = (value, self.position[1], self.position[2])
     
     @y.setter
-    def y(self, value: float):
-        self.position[1] = value
+    def y(self, value: float) -> None:
+        if len(self.position) == 2:
+            self.position = (self.position[0], value)
+        else:
+            self.position = (self.position[0], value, self.position[2])
     
     @z.setter
-    def z(self, value: float):
-        self.position[2] = value
+    def z(self, value: float) -> None:
+        self.position = (self.position[0], self.position[1], value)
+    
+    @r.setter
+    def r(self, value: float) -> None:
+        if len(self.position) == 2: # Polar
+            if math.isnan(self.phi) and value != 0:
+                raise ValueError("Cannot set r to anything except zero if phi"
+                                 + " is nan, change r or both simultaneously")
+            elif math.isnan(self.phi) and value == 0:
+                self.position = (0, 0)
+            else:
+                self.position = (
+                    value * math.cos(self.phi), value * math.sin(self.phi)
+                )
+        else: # Spherical (or invalid)
+            self.position = (
+                value * math.sin(self.theta) * math.cos(self.phi),
+                value * math.sin(self.theta) * math.sin(self.phi),
+                value * self.cos(self.theta)
+            )
+    
+    @phi.setter
+    def phi(self, value: float) -> None:
+        if len(self.position) == 2: # Polar
+            if math.isnan(value) or self.r == 0:
+                self.position = (0, 0)
+            else:
+                self.position = (
+                    self.r * math.cos(value), self.r * math.sin(value)
+                )
+        else: # Spherical (or invalid)
+            if math.isnan(value) and self.z > 0:
+                self.position = (0, 0, self.r)
+            elif math.isnan(value) and self.z < 0:
+                self.position = (0, 0, -self.r)
+            elif self.r == 0:
+                self.position = (0, 0, 0)
+            else:
+                self.position = (
+                    self.r * math.sin(self.theta) * math.cos(value),
+                    self.r * math.sin(self.theta) * math.sin(value),
+                    self.r * self.cos(self.theta)
+                )
+    
+    @theta.setter
+    def theta(self, value: float) -> None:
+        if self.r == 0 or (math.isnan(self.phi) and math.isnan(value)):
+            self.position = (0, 0, 0)
+        elif math.isnan(self.phi) and abs(value) == math.pi/2:
+            self.position = (0, 0, self.r * math.cos(value))
+        elif math.isnan(self.phi):
+            raise ValueError("Cannot set theta to anything except +/-pi/2 if "
+                             + "phi is nan, change phi or both simultaneously")
+        else:
+            self.position = (
+                self.r * math.sin(value) * math.cos(self.phi),
+                self.r * math.sin(value) * math.sin(self.phi),
+                self.r * math.cos(value)
+            )
     
     # Public Methods #
+    def phi_degrees(self) -> float:
+        """Returns the polar/spherical azimuth coordinate of the point in 
+        degrees."""
+        return math.degrees(self.phi)
+    
+    def theta_degrees(self) -> float:
+        """Returns the spherical inclination coordinate of the point in 
+        degrees."""
+        return math.degrees(self.theta)
+    
+    def set_phi_degrees(self, value: float) -> None:
+        """Sets the polar/spherical azimuth coordinate of the point using a 
+        value in degrees."""
+        self.phi = math.radians(value)
+    
+    def set_theta_degrees(self, value: float) -> None:
+        """Sets the spherical inclindation coordinate of the point using a 
+        value in degrees."""
+        self.phi = math.radians(value)
+    
     def vector(self, vertical = True) -> np.ndarray:
         """Returns a numpy vector of the point's position"""
         array = np.array(self) 
@@ -74,7 +217,7 @@ class Point:
         self.dimension = 0
         return self
     
-    def __next__(self):
+    def __next__(self) -> float:
         if self.dimension < len(self.position):
             i = self.dimension
             self.dimension += 1
@@ -82,9 +225,9 @@ class Point:
         else:
             raise StopIteration
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"PanCAD Point at position {self.position}"
     
     # NumPy Dunders #
-    def __array__(self, dtype=None, copy=None):
+    def __array__(self, dtype=None, copy=None) -> np.ndarray:
         return np.array(list(self))
--- tests/test_geometry_point.py
+++ tests/test_geometry_point.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import unittest
 from xml.etree import ElementTree as ET
 import os
+import math
 
 import numpy as np
 
@@ -46,26 +47,181 @@ class TestPointInit(unittest.TestCase):
     def test_vector(self):
         pass
 
-class TestPointFunctions(unittest.TestCase):
+class TestPointPropertiesFunctions(unittest.TestCase):
     
     def setUp(self):
         self.pt = Point()
-        self.coordinates = coordinates = [
-            (0, 0, 0),
-            (1, 1, 1),
-            (1, 1),
+        self.coordinates = [
+            ((0, 0, 0), 0, math.nan, math.nan),
+            ((1, 1, 0), math.sqrt(2), math.radians(45), math.radians(90)),
+            ((-1, 1, 0), math.sqrt(2), math.radians(135), math.radians(90)),
+            ((-1, -1, 0), math.sqrt(2), -math.radians(135), math.radians(90)),
+            ((1, -1, 0), math.sqrt(2), -math.radians(45), math.radians(90)),
+            ((1, 0, 0), 1, math.radians(0), math.radians(90)), 
+            ((0, 1, 0), 1, math.radians(90), math.radians(90)), 
+            ((-1, 0, 0), 1, math.radians(180), math.radians(90)), 
+            ((0, -1, 0), 1, -math.radians(90), math.radians(90)),
+            ((1, 1, 1), math.sqrt(3), math.radians(45), math.atan(math.hypot(1,1)/1)),
+            ((1, 1, -1), math.sqrt(3), math.radians(45), math.pi + math.atan(math.hypot(1,1)/-1)),
+            ((0, 0, 1), math.sqrt(1), math.nan, math.atan(math.hypot(0,0)/1)),
+            ((0, 0, -1), math.sqrt(1), math.nan, math.pi + math.atan(math.hypot(0,0)/-1)),
         ]
-    
-    def test_set_position(self):
+        
+        self.coordinates2d = []
         for coordinate in self.coordinates:
+            self.coordinates2d.append(
+                (coordinate[0][:2],
+                 math.hypot(coordinate[0][0], coordinate[0][1]),
+                 coordinate[2])
+            )
+        self.coordinates_polar = []
+        for coordinate in self.coordinates2d:
+            self.coordinates_polar.append(
+                (
+                    (self.coordinates2d[1], self.coordinates2d[2]),
+                    self.coordinates2d[0]
+                )
+            )
+        self.coordinates_spherical = []
+        for coordinate in self.coordinates:
+            self.coordinates_spherical.append(
+                ((coordinate[1], coordinate[2], coordinate[3]), coordinate[0])
+            )
+        
+    def test_position_setter(self):
+        for coordinate, *_ in self.coordinates:
             with self.subTest(coordinate = coordinate):
                 self.pt.position = coordinate
-                self.assertEqual(self.pt.position, coordinate)
+                self.assertCountEqual(self.pt.position, coordinate)
     
-    def test_get_xyz(self):
-        pass
+    def test_xy_getters(self):
+        for coordinate, *_ in self.coordinates2d:
+            with self.subTest(coordinate = coordinate):
+                self.pt.position = coordinate
+                xy = (self.pt.x, self.pt.y)
+                self.assertCountEqual(xy, coordinate)
+    
+    def test_xyz_getters(self):
+        for coordinate, *_ in self.coordinates:
+            with self.subTest(coordinate = coordinate):
+                self.pt.position = coordinate
+                xyz = (self.pt.x, self.pt.y, self.pt.z)
+                self.assertCountEqual(xyz, coordinate)
+    
+    def test_r_getter(self):
+        for coordinate, expected_r, *_ in self.coordinates:
+            with self.subTest(test=[coordinate, expected_r]):
+                self.pt.position = coordinate
+                self.assertEqual(self.pt.r, expected_r)
+    
+    def test_phi_getter(self):
+        for coordinate, _, expected_phi in self.coordinates2d:
+            with self.subTest(test=[
+                    coordinate,
+                    f"{math.degrees(expected_phi)}°, {expected_phi} radians"
+                ]):
+                self.pt.position = coordinate
+                if coordinate == (0, 0):
+                    self.assertTrue(math.isnan(self.pt.phi))
+                else:
+                    self.assertEqual(self.pt.phi, expected_phi)
+    
+    def test_theta_getter(self):
+        for coordinate, _, _, expected_theta in self.coordinates:
+            with self.subTest(test=[
+                    coordinate,
+                    f"{math.degrees(expected_theta)}°, {expected_theta} radians"
+                ]):
+                self.pt.position = coordinate
+                if coordinate == (0, 0, 0):
+                    self.assertTrue(math.isnan(self.pt.theta))
+                else:
+                    self.assertEqual(self.pt.theta, expected_theta)
+    
+    def test_polar_getter(self):
+        for coordinate, expected_r, expected_phi, *_ in self.coordinates2d:
+            with self.subTest(test=[
+                    coordinate,
+                    expected_r,
+                    f"{math.degrees(expected_phi)}°, {expected_phi} radians"
+                ]):
+                self.pt.position = coordinate
+                if coordinate == (0, 0):
+                    self.assertTrue(math.isnan(self.pt.phi))
+                    self.assertEqual(self.pt.r, expected_r)
+                else:
+                    self.assertEqual(self.pt.polar, (expected_r, expected_phi))
+    
+    def test_spherical_getter(self):
+        for (coordinate, expected_r,
+             expected_phi, expected_theta) in self.coordinates:
+            with self.subTest(test=[
+                    coordinate,
+                    expected_r,
+                    f"{math.degrees(expected_phi)}°, {expected_phi} radians",
+                    f"{math.degrees(expected_theta)}°, {expected_theta} radians"
+                ]):
+                self.pt.position = coordinate
+                if coordinate == (0, 0, 0):
+                    self.assertTrue(math.isnan(self.pt.phi))
+                    self.assertTrue(math.isnan(self.pt.theta))
+                    self.assertEqual(self.pt.r, expected_r)
+                elif coordinate[:2] == (0, 0):
+                    self.assertTrue(math.isnan(self.pt.phi))
+                    self.assertEqual(
+                        (self.pt.spherical[0], None, self.pt.spherical[2]),
+                        (expected_r, None, expected_theta)
+                    )
+                else:
+                    self.assertEqual(
+                        self.pt.spherical,
+                        (expected_r, expected_phi, expected_theta)
+                    )
+    
+    def test_xy_setters(self):
+        new_coordinate = (1, 2)
+        self.pt.position = (0, 0)
+        self.pt.x, self.pt.y = new_coordinate[0], new_coordinate[1]
+        self.assertCountEqual(self.pt.position, new_coordinate)
+    
+    def test_xyz_setters(self):
+        new_coordinate = (1, 2, 3)
+        self.pt.position = (0, 0)
+        self.pt.x = new_coordinate[0]
+        self.pt.y = new_coordinate[1]
+        self.pt.z = new_coordinate[2]
+        self.assertCountEqual(self.pt.position, new_coordinate)
+    
+    def test_vector(self):
+        tests = []
+        HORIZONTAL, VERTICAL = False, True
+        for coordinate, *_ in self.coordinates: 
+            tests.append(
+                (coordinate,
+                 HORIZONTAL,
+                 np.array(coordinate))
+            )
+            tests.append(
+                (coordinate,
+                 VERTICAL,
+                 np.array(coordinate).reshape(len(coordinate), 1))
+            )
+        
+        for coordinate, orientation, expected in tests:
+            with self.subTest(test = [coordinate, orientation, expected]):
+                self.pt.position = coordinate
+                self.assertTrue(
+                    self.pt.vector(orientation).shape == expected.shape
+                )
+    
+    # def test_polar_setter(self):
+        # for polar_coordinate, xy_coordinate in self.coordinates_polar:
+            # with self.subTest(test=[polar_coordinate, xy_coordinate]):
+                # self.pt.polar
+
 
 if __name__ == "__main__":
-    with open("tests/logs/"+ Path(sys.modules[__name__].__file__).stem+".log", "w") as f:
+    with open("tests/logs/" + Path(sys.modules[__name__].__file__).stem
+              +".log", "w") as f:
         f.write("finished")
     unittest.main()
