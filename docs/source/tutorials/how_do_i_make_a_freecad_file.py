1e7a9514884e87a5c535d3ef1b23a45add501cf0
unknown <georgesparks673@gmail.com>
2025-08-26 08:32:48 -0500

    finished making a cube tutorial. Added pictures and tutorial code to flesh the tutorial out.

--- docs/source/tutorials/how_do_i_make_a_freecad_file.py
+++ docs/source/tutorials/how_do_i_make_a_freecad_file.py
@@ -1,57 +1,78 @@
-from PanCAD import PartFile
-from PanCAD.geometry import LineSegment, Sketch, Extrude
-from PanCAD.geometry.constraints import (Coincident,
-                                         Distance,
-                                         Horizontal,
-                                         Vertical)
-from PanCAD.geometry.constants import ConstraintReference as CR
-
-filename = "tutorial_cube"
-side = 1
-unit = "mm"
-sketch_name = "square_sketch"
-
+# [corner-definition-start]
+side_length = 1
 bottom_left = (0, 0)
-bottom_right = (side, 0)
-top_right = (side, side)
-top_left = (0, side)
+bottom_right = (side_length, 0)
+top_right = (side_length, side_length)
+top_left = (0, side_length)
+# [corner-definition-end]
 
+# [line-definition-start]
+from PanCAD.geometry import LineSegment
 bottom = LineSegment(bottom_left, bottom_right)
 right = LineSegment(bottom_right, top_right)
 top = LineSegment(top_right, top_left)
 left = LineSegment(top_left, bottom_left)
+# [line-definition-end]
 
-sketch = Sketch(plane_reference=CR.XY,
-                geometry=[bottom, right, top, left],
+# [sketch-definition-start]
+from PanCAD.geometry import Sketch
+sketch = Sketch(geometry=[bottom, right, top, left],
                 uid="square_sketch")
+# [sketch-definition-end]
 
+# from PanCAD import PartFile
+# filename = "tutorial_cube"
+# sketch_name = "square_sketch"
+
+# [constraint-definition-start]
+from PanCAD.geometry.constants import ConstraintReference
+from PanCAD.geometry.constraints import (Coincident, Distance,
+                                         Horizontal, Vertical)
 sketch.constraints = [
-    # Line Segment Orientations
-    Horizontal(bottom, CR.CORE),
-    Horizontal(top, CR.CORE),
-    Vertical(right, CR.CORE),
-    Vertical(left, CR.CORE),
-    
     # Corner Coincidence
-    Coincident(bottom, CR.START, left, CR.END),
-    Coincident(bottom, CR.END, right, CR.START),
-    Coincident(right, CR.END, top, CR.START),
-    Coincident(left, CR.START, top, CR.END),
+    Coincident(bottom, ConstraintReference.START,
+               left, ConstraintReference.END),
+    Coincident(bottom, ConstraintReference.END,
+               right, ConstraintReference.START),
+    Coincident(right, ConstraintReference.END,
+               top, ConstraintReference.START),
+    Coincident(left, ConstraintReference.START,
+               top, ConstraintReference.END),
+    
+    # Line Segment Orientations
+    Horizontal(bottom, ConstraintReference.CORE),
+    Horizontal(top, ConstraintReference.CORE),
+    Vertical(right, ConstraintReference.CORE),
+    Vertical(left, ConstraintReference.CORE),
+    
+    # Top/Bottom and Left/Right Distances
+    Distance(bottom, ConstraintReference.CORE,
+             top, ConstraintReference.CORE,
+             side_length, unit="mm"),
+    Distance(left, ConstraintReference.CORE,
+             right, ConstraintReference.CORE,
+             side_length, unit="mm"),
     
     # Constrain one corner to the origin
-    Coincident(bottom, CR.START,
-               sketch.get_sketch_coordinate_system(), CR.ORIGIN),
-    
-    # Height and Width
-    Distance(bottom, CR.CORE, top, CR.CORE, side, unit=unit),
-    Distance(left, CR.CORE, right, CR.CORE, side, unit=unit),
+    Coincident(bottom,
+               ConstraintReference.START,
+               sketch.get_sketch_coordinate_system(),
+               ConstraintReference.ORIGIN),
 ]
-extrude = Extrude.from_length(sketch, side, "cube_extrude")
+# [constraint-definition-end]
 
+# [extrude-definition-start]
+from PanCAD.geometry import Extrude
+extrude = Extrude.from_length(sketch, side_length, "cube_extrude")
+# [extrude-definition-end]
+
+# [file-definition-start]
+from PanCAD import PartFile
 file = PartFile("tutorial_cube")
 file.add_feature(sketch)
 file.add_feature(extrude)
-file.to_freecad(file.filename + ".FCStd")
+file.to_freecad("tutorial_cube.FCStd")
+# [file-definition-end]
 
 import os
 os.remove(file.filename + ".FCStd")
