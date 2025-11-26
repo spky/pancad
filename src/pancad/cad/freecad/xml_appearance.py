5d7ffa5c8f7add26778ec5f8610e033b3a708a05
unknown <georgesparks673@gmail.com>
2025-11-26 07:09:38 -0600

    added ability to read shape and color arrays

---
+++ src/pancad/cad/freecad/xml_appearance.py
@@ -0,0 +1,84 @@
+"""A module providing functions for reading and writing FreeCAD appearance 
+files.
+"""
+from __future__ import annotations
+
+from itertools import islice
+from typing import TYPE_CHECKING
+import struct
+
+
+if TYPE_CHECKING:
+    from zipfile import ZipFile, ZipInfo
+
+def batched(iterable, n, *, strict=False):
+    # batched('ABCDEFG', 2) → AB CD EF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    iterator = iter(iterable)
+    while batch := tuple(islice(iterator, n)):
+        if strict and len(batch) != n:
+            raise ValueError('batched(): incomplete batch')
+        yield batch
+
+def read_shape_appearance(archive: ZipFile,
+                          filename: str | ZipInfo
+                          ) -> dict[str, float | tuple[int]]:
+    """Reads shape appearance file data.
+    
+    :param archive: A ZipFile of a FreeCAD file.
+    :param filename: The name of the ShapeAppearance file to read.
+    :returns: A dictionary of labels to either their float values or their RGBA 
+        color integer tuples.
+    """
+    with archive.open(filename) as file:
+        data = bytes(file.read())
+    
+    parsed = {}
+    
+    if len(data) > 40:
+        parsed["uid"] = data[40:].decode()
+        data = data[:40]
+    
+    LABELS = [
+        "header",
+        "ambient",
+        "diffuse",
+        "specular",
+        "emissive",
+        "shininess",
+        "transparency",
+        "blank0",
+        "blank1",
+        "uuid_sync",
+    ]
+    COLORS = ["ambient", "diffuse", "specular", "emissive"]
+    FLOATS = ["shininess", "transparency"]
+    for label, entry in zip(LABELS, batched(data, 4)):
+        if label in COLORS:
+            parsed[label] = entry[::-1]
+        elif label in FLOATS:
+            parsed[label] = struct.unpack("<f", bytes(entry))[0]
+        elif label == "header":
+            assert entry == (1, 0, 0, 0)
+        elif label == "uuid_sync":
+            assert entry == (0, 0, 0, 0) or entry == (36, 0, 0, 0)
+        elif label.startswith("blank"):
+            assert entry == (0, 0, 0, 0)
+        else:
+            raise ValueError(f"Unhandled label {label}")
+    return parsed
+
+def read_color_array(archive: ZipFile, filename: str | ZipInfo) -> tuple[int]:
+    """Reads color array file data.
+    
+    :param archive: A ZipFile of a FreeCAD file.
+    :param filename: The name of the Point or Line ColorArray file to read.
+    :returns: A tuple of ARGB integer values.
+    """
+    with archive.open(filename) as file:
+        data = bytes(file.read())
+    
+    header, color = list(batched(data, 4))
+    assert header == (1, 0, 0, 0)
+    return color
--- src/pancad/cad/freecad/xml_properties.py
+++ src/pancad/cad/freecad/xml_properties.py
@@ -2,9 +2,12 @@
 properties from/to its save files.
 """
 from __future__ import annotations
+from functools import partial
 
+from collections import namedtuple
 import logging
 from typing import TYPE_CHECKING, Any
+from uuid import UUID
 from xml.etree import ElementTree as ET
 
 import numpy as np
@@ -17,9 +20,35 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
+ARGB = namedtuple("ARGB", ["a", "r", "g", "b"])
+
+def unpack_argb(packed: int) -> ARGB:
+    """Returns a tuple of ARGB values from an integer."""
+    return ARGB(*[(packed >> shift_by) & 0xFF for shift_by in [24, 16, 8, 0]])
+
+def _read_bad_type(element: Element) -> list[tuple[bool, int, float]]:
+    layer_list = element.find(XMLTag.VISUAL_LAYER_LIST)
+    if layer_list.tag != XMLTag.VISUAL_LAYER_LIST:
+        raise ValueError(f"Unexpected tag: {element.tag}")
+    layers = []
+    for list_ in layer_list.iter(XMLTag.VISUAL_LAYER):
+        visible = list_.attrib[XMLAttr.VISIBLE] == "true"
+        line_pattern = int(list_.attrib[XMLAttr.LINE_PATTERN])
+        line_width = float(list_.attrib[XMLAttr.LINE_WIDTH])
+        layers.append((visible, line_pattern, line_width))
+    return layers
+    
+
 def _read_bool(element: Element) -> bool:
     if len(element) != 1: raise ValueError(f"Found {len(element)}")
     return element.find(XMLTag.BOOL).attrib[XMLAttr.VALUE] == "true"
+
+def _read_color(element: Element) -> tuple[int, int, int, int]:
+    """Returns the ARGB values from the integer in the element as a tuple."""
+    if len(element) != 1: raise ValueError(f"Found {len(element)}")
+    ARGB = namedtuple("ARGB", ["a", "r", "g", "b"])
+    integer = int(element.find(XMLTag.PROPERTY_COLOR).attrib[XMLAttr.VALUE])
+    return unpack_argb(integer)
 
 def _read_constraint_list(element: Element) -> list[dict]:
     if len(element) != 1: raise ValueError(f"Found {len(element)}")
@@ -91,6 +120,10 @@ def _read_geometry_list(element: Element) -> dict[int, dict]:
         }
     return geometries
 
+def _read_integer(element: Element) -> int:
+    if len(element) != 1: raise ValueError(f"Found {len(element)}")
+    return int(element.find(XMLTag.INTEGER).attrib[XMLAttr.VALUE])
+
 def _read_link_sublist(element: Element) -> list[tuple[str, str]]:
     """Returns the name and sub in each link in the link sublist"""
     if len(element) != 1: raise ValueError(f"Found {len(element)}")
@@ -110,6 +143,30 @@ def _read_link_list(element: Element) -> list:
 def _read_map(element: Element) -> list:
     raise NotImplementedError("Map not implemented yet")
 
+def _read_material(element: Element) -> dict[str, ARGB | float | str | UUID]:
+    FUNC_DISPATCH = {
+        lambda value: unpack_argb(int(value)): [
+            "ambientColor", "diffuseColor", "specularColor", "emissiveColor",
+        ],
+        float: ["shininess", "transparency"],
+        lambda value: UUID if value else "": ["uuid",]
+    }
+    dispatch = {}
+    for func, fields in FUNC_DISPATCH.items():
+        dispatch.update({field: func for field in fields})
+    material_element = element.find(XMLTag.PROPERTY_MATERIAL)
+    material = {}
+    for field, value in dict(material_element.attrib).items():
+        if field in dispatch:
+            material[field] = dispatch[field](value)
+        else:
+            material[field] = value
+    return material
+
+def _read_material_list(element: Element) -> dict[str, str]:
+    list_element = element.find(XMLTag.MATERIAL_LIST)
+    return dict(list_element.attrib)
+
 def _read_expression_engine(element: Element) -> list:
     raise NotImplementedError("Expression Engine not implemented yet")
 
@@ -125,40 +182,66 @@ def _read_placement(element: Element) -> tuple[tuple[float], np.quaternion]:
     
     return tuple(position), np.quaternion(*quat_params)
 
-def _read_string(element: Element) -> str:
+def _read_python_object(element: Element) -> dict[str, str]:
     if len(element) != 1: raise ValueError(f"Found {len(element)}")
-    return element.find(XMLTag.STRING).attrib[XMLAttr.VALUE]
+    return dict(element.find(XMLTag.PYTHON).attrib)
 
-def _read_uuid(element: Element) -> UUID:
+def _read_nested_string(element: Element, tag: XMLTag, field: XMLAttr):
+    """Used to read nested strings that aren't necessarily in a String tag."""
     if len(element) != 1: raise ValueError(f"Found {len(element)}")
-    return element.find(XMLTag.UUID).attrib[XMLAttr.VALUE]
+    return element.find(tag).attrib[field]
+
+_read_string = partial(_read_nested_string,
+                       tag=XMLTag.STRING, field=XMLAttr.VALUE)
+_read_color_list = partial(_read_nested_string,
+                           tag=XMLTag.COLOR_LIST, field=XMLAttr.FILE)
+
+def _read_uuid(element: Element) -> UUID | None:
+    if len(element) != 1: raise ValueError(f"Found {len(element)}")
+    return UUID(element.find(XMLTag.UUID).attrib[XMLAttr.VALUE])
+
+def _read_vector(element: Element) -> tuple[float, float, float]:
+    if len(element) != 1: raise ValueError(f"Found {len(element)}")
+    XYZ_FIELDS = ["valueX", "valueY", "valueZ"]
+    vector = element.find(XMLTag.PROPERTY_VECTOR)
+    return tuple(float(vector.attrib[component]) for component in XYZ_FIELDS)
 
 PROPERTY_DISPATCH = {
+        XMLPropertyType.APP_ANGLE: _read_float,
         XMLPropertyType.APP_BOOL: _read_bool,
+        XMLPropertyType.APP_COLOR: _read_color,
+        XMLPropertyType.APP_COLOR_LIST: _read_color_list,
         XMLPropertyType.APP_ENUM: _read_enum,
         XMLPropertyType.APP_FLOAT: _read_float,
+        XMLPropertyType.APP_FLOAT_CONSTRAINT: _read_float,
+        XMLPropertyType.APP_LENGTH: _read_float,
         XMLPropertyType.APP_LINK_SUBLIST: _read_link_sublist,
         XMLPropertyType.APP_LINK_LIST_HIDDEN: _read_link_list,
+        XMLPropertyType.APP_MATERIAL: _read_material,
+        XMLPropertyType.APP_MATERIAL_LIST: _read_material_list,
         XMLPropertyType.APP_PLACEMENT: _read_placement,
+        XMLPropertyType.APP_PYTHON_OBJECT: _read_python_object,
+        XMLPropertyType.APP_PERCENT: _read_integer,
         XMLPropertyType.APP_PRECISION: _read_float,
+        XMLPropertyType.APP_VECTOR: _read_vector,
         XMLPropertyType.APP_STRING: _read_string,
         XMLPropertyType.APP_UUID: _read_uuid,
+        
+        XMLPropertyType.BAD_TYPE: _read_bad_type,
         
         XMLPropertyType.PART_GEOMETRY_LIST: _read_geometry_list,
         
         XMLPropertyType.SKETCHER_CONSTRAINT_LIST: _read_constraint_list,
 }
 
-def read_properties(element: Element,
-                    tag: XMLTag) -> list[tuple[str, str, int, Any]]:
+def read_properties(element: Element) -> list[tuple[str, str, int, Any]]:
     """Reads the property formatted tags under the element.
     
     :param element: The Properties element with elements underneath.
-    :param tag: The subelement tag to search for.
     :returns: A list of (Name, Type, Status, Value) tuples.
     """
     properties = []
-    for property_ in element.iter(tag):
+    for property_ in element.iter(XMLTag.PROPERTY):
         name = property_.get(XMLAttr.NAME)
         type_ = property_.get(XMLAttr.TYPE)
         if (status := property_.get(XMLAttr.STATUS)) is not None: 
@@ -173,19 +256,3 @@ def read_properties(element: Element,
         properties.append((name, type_, status, value))
     return properties
 
-def read_private_properties(element: Element,
-                            tag: XMLTag) -> list[tuple[str, str, int]]:
-    """Reads the _Property like formatted tags under the element.
-    
-    :param element: The Properties element with elements underneath.
-    :param tag: The subelement tag to search for.
-    :returns: A list of (Name, Type, Status) tuples.
-    """
-    properties = []
-    for property_ in element.iter(tag):
-        name = property_.get(XMLAttr.NAME)
-        type_ = property_.get(XMLAttr.TYPE)
-        if (status := property_.get(XMLAttr.STATUS)) is not None: 
-            status = int(status)
-        properties.append((name, type_, status))
-    return properties
