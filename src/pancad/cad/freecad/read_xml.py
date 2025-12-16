a21983d2d441c369b3158b0ef2f0ceab6d876646
unknown <georgesparks673@gmail.com>
2025-12-16 10:55:39 -0600

    linted read_xml.py - Moved most constants up to top as all-caps constants

--- src/pancad/cad/freecad/read_xml.py
+++ src/pancad/cad/freecad/read_xml.py
@@ -21,17 +21,30 @@ if TYPE_CHECKING:
     from xml.etree.ElementTree import Element, ElementTree
     from .constants.archive_constants import App, PartDesign
 
+@cache
+def _xml_config() -> dict[str, str]:
+    """Returns the xml configuration dict of the freecad.toml file"""
+    with open(Path(resources.__file__).parent / FREECAD_TOML, "rb") as file:
+        return tomllib.load(file)["xml"]
+
 logger = logging.getLogger(__name__)
+FREECAD_TOML = "freecad.toml"
+XML_CONFIG = _xml_config()
+XPATHS = XML_CONFIG["xpaths"]
+FIELDS = XML_CONFIG["pancad"]["fields"]
+SHARED = XML_CONFIG["shared"]
+SKETCH_XPATH = XPATHS["OBJECT_TYPE"].format(Sketcher.SKETCH)
+GEO_LIST_XPATH = XPATHS["PROPERTY_TYPE"].format(Part.GEOMETRY_LIST)
+GEO_EXT_XPATH = XPATHS["GEOMETRY_EXT"].format(Sketcher.GEOMETRY_EXT)
 
 def camera(tree: ElementTree) -> dict[str, str]:
     """Reads the camera settings of an FCStd file from the GuiDocument.xml."""
-    type_field = _xml_config()["pancad"]["fields"]["camera_type"]
-    
+    type_field = XML_CONFIG["pancad"]["fields"]["camera_type"]
     element = tree.find(Tag.CAMERA)
     settings = element.get(Attr.SETTINGS)
     lines = settings.split("\n")
-    IGNORE = ["", "}"]
-    lines = [line.strip() for line in settings.split("\n") if line not in IGNORE]
+    lines = [line.strip() for line in settings.split("\n")
+             if line not in ["", "}"]]
     data = {}
     data[type_field] = lines.pop(0).split()[0]
     for line in lines:
@@ -42,64 +55,43 @@ def camera(tree: ElementTree) -> dict[str, str]:
 
 def dependencies(tree: str) -> list[dict[str, str]]:
     """Returns a list of all object dependency relations in the tree as dicts."""
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    fields = config["pancad"]["fields"]
-    
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
     data = []
-    for deps in tree.findall(xpaths["OBJECT_DEPENDENCIES"]):
+    for deps in tree.findall(XPATHS["OBJECT_DEPENDENCIES"]):
         name = deps.get(Attr.NAME_CAPITALIZED)
         for dep in deps.iter(Tag.DEP):
             data.append(
                 {
-                    fields["file_uid"]: file_uid,
+                    FIELDS["file_uid"]: file_uid,
                     Attr.NAME: name,
-                    fields["depends_on"]: dep.get(Attr.NAME_CAPITALIZED),
+                    FIELDS["depends_on"]: dep.get(Attr.NAME_CAPITALIZED),
                 }
             )
     return data
 
-def expand(expand: Element) -> set[str]:
+def expand(element: Element) -> set[str]:
     """Recursively reads the names that are expanded in an expand xml element."""
     all_expanded = set()
-    if name := expand.get(Attr.NAME):
+    if name := element.get(Attr.NAME):
         all_expanded.add(name)
-    for sub in expand.findall(Tag.EXPAND):
-        for sub_name in read_expand(sub):
+    for sub in element.findall(Tag.EXPAND):
+        for sub_name in expand(sub):
             all_expanded.add(sub_name)
     return all_expanded
 
-def extensions(element: Element) -> list[tuple[str, str]]:
-    """Finds and reads an Extensions element and provides a list of (type, name) 
-    tuples for each one.
-    """
-    data = []
-    if not (element := extendable.find(Tag.EXTENSIONS)):
-        return data # Return empty list of no extensions found
-    for ext in element.iter(Tag.EXTENSION):
-        data.append((ext.get(Attr.TYPE), ext.get(Attr.NAME)))
-    return data
-
 def metadata(tree: ElementTree) -> list[dict[str, FreecadPropertyValueType]]:
     """Returns a tuple of field names and a tuple of values for FCStd metadata."""
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    fields = config["pancad"]["fields"]
-    
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
     data = []
-    for element in tree.findall(xpaths["PROPERTY"]):
+    for element in tree.findall(XPATHS["PROPERTY"]):
         try:
             value = read_property_value(element)
         except FreecadUnsupportedPropertyError as err:
-            logger.warning(f"Unsupported metadata {err}")
+            logger.warning("Unsupported metadata %s", err)
             continue
         data.append(
             {
-                fields["file_uid"]: file_uid,
+                FIELDS["file_uid"]: file_uid,
                 Attr.NAME: element.get(Attr.NAME),
                 Attr.TYPE: element.get(Attr.TYPE),
                 Attr.STATUS: element.get(Attr.STATUS),
@@ -112,59 +104,47 @@ def object_info(tree: ElementTree) -> list[dict[str, FreecadPropertyValueType]]:
     """Returns a tuple of field names and a tuple of values for fields common to 
     all objects in an FCStd document.xml tree.
     """
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    
-    file_uid_field = config["pancad"]["fields"]["file_uid"]
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    shared_attributes = tuple(config["shared"]["object_attributes"])
-    shared_properties = tuple(config["shared"]["object_properties"])
-    
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
     info = []
-    for object_ in tree.findall(xpaths["OBJECTS"]):
+    for object_ in tree.findall(XPATHS["OBJECTS"]):
         object_name = object_.get(Attr.NAME)
-        properties = {attr: object_.get(attr) for attr in shared_attributes}
-        object_data = tree.find(xpaths["OBJECT_DATA"].format(object_name))
-        for name in shared_properties:
-            property_ = object_data.find(xpaths["PROPERTY_NAME"].format(name))
+        properties = {attr: object_.get(attr)
+                      for attr in SHARED["object_attributes"]}
+        object_data = tree.find(XPATHS["OBJECT_DATA"].format(object_name))
+        for name in SHARED["object_properties"]:
+            property_ = object_data.find(XPATHS["PROPERTY_NAME"].format(name))
             value = read_property_value(property_)
             properties[name] = value
-        info.append({file_uid_field: file_uid, **properties})
+        info.append({FIELDS["file_uid"]: file_uid, **properties})
     return info
 
 def object_type(tree: ElementTree,
                 type_: App | Sketcher | PartDesign
                 ) -> list[dict[str, FreecadPropertyValueType]]:
     """Reads the properties all objects of a type from a FCStd file."""
-    SKIP_TYPES = [Part.GEOMETRY_LIST, Sketcher.CONSTRAINT_LIST]
     # These types will be read by other functions
-    
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    file_uid_field = config["pancad"]["fields"]["file_uid"]
-    
-    static_fields = (file_uid_field, *config["shared"]["object_attributes"])
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    
+    static_fields = (FIELDS["file_uid"], *SHARED["object_attributes"])
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
     # Read data into dicts
     data = []
-    for object_ in tree.findall(xpaths["OBJECT_TYPE"].format(type_)):
+    for object_ in tree.findall(XPATHS["OBJECT_TYPE"].format(type_)):
         object_name = object_.attrib[Attr.NAME]
-        properties = {file_uid_field: file_uid,
+        properties = {FIELDS["file_uid"]: file_uid,
                       Attr.NAME: object_name,
                       Attr.ID: object_.attrib[Attr.ID]}
         properties.update({attr: object_.get(attr) for attr in static_fields
                            if attr not in properties})
-        
-        object_data = tree.find(xpaths["OBJECT_DATA"].format(object_name))
-        for property_ in object_data.findall(xpaths["PROPERTY"]):
-            if property_.get(Attr.TYPE) in SKIP_TYPES:
+        object_data = tree.find(XPATHS["OBJECT_DATA"].format(object_name))
+        for property_ in object_data.findall(XPATHS["PROPERTY"]):
+            if property_.get(Attr.TYPE) in [Part.GEOMETRY_LIST,
+                                            Sketcher.CONSTRAINT_LIST]:
                 continue
             property_name = property_.get(Attr.NAME)
             try:
                 properties[property_name] = read_property_value(property_)
             except FreecadUnsupportedPropertyError as err:
-                logger.warning(f"Skip {property_name} on {object_name}: {err}")
+                logger.warning("Skip %s on %s: %s",
+                               property_name, object_name, err)
                 properties[property_name] = err.__class__.__name__
         data.append(properties)
     return data
@@ -176,21 +156,14 @@ def object_types(tree: ElementTree) -> list[str]:
 
 def sketch_constraints(tree: ElementTree) -> list[dict[str, str]]:
     """Returns a tuple of field names and a tuple of values for each constraint"""
-    fields = _xml_config()["pancad"]["fields"]
-    static_fields = (fields["file_uid"], fields["sketch_name"])
-    
-    xpaths = _xml_config()["xpaths"]
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    sketch_xpath = xpaths["OBJECT_TYPE"].format(Sketcher.SKETCH)
-    list_xpath = xpaths["PROPERTY_TYPE"].format(Sketcher.CONSTRAINT_LIST)
-    
-    dynamic_fields = []
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
+    list_xpath = XPATHS["PROPERTY_TYPE"].format(Sketcher.CONSTRAINT_LIST)
     data = []
-    for sketch in tree.findall(sketch_xpath):
+    for sketch in tree.findall(SKETCH_XPATH):
         sketch_name = sketch.get(Attr.NAME)
-        sketch_data = tree.find(xpaths["OBJECT_DATA"].format(sketch_name))
-        static = {fields["file_uid"]: file_uid,
-                  fields["sketch_name"]: sketch_name}
+        sketch_data = tree.find(XPATHS["OBJECT_DATA"].format(sketch_name))
+        static = {FIELDS["file_uid"]: file_uid,
+                  FIELDS["sketch_name"]: sketch_name}
         for property_ in sketch_data.findall(list_xpath):
             constraints = read_property_value(property_)
             for info in constraints:
@@ -204,35 +177,22 @@ def sketch_geometry(tree: ElementTree, type_: Part) -> list[dict[str, str]]:
     :param tree: An ElementTree of the document.xml file.
     :param type_: A type attribute value of a parent Geometry tag.
     """
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    fields = config["pancad"]["fields"]
-    
-    sketch_xpath = xpaths["OBJECT_TYPE"].format(Sketcher.SKETCH)
-    geo_list_xpath = xpaths["PROPERTY_TYPE"].format(Part.GEOMETRY_LIST)
-    geo_ext_xpath = xpaths["GEOMETRY_EXT"].format(Sketcher.GEOMETRY_EXT)
-    geo_type_xpath = xpaths["GEOMETRY_TYPE"].format(type_)
-    
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    
-    dynamic_fields = []
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
+    geo_type_xpath = XPATHS["GEOMETRY_TYPE"].format(type_)
     data = []
-    for sketch in tree.findall(sketch_xpath):
+    for sketch in tree.findall(SKETCH_XPATH):
         sketch_name = sketch.get(Attr.NAME)
-        sketch_data = tree.find(xpaths["OBJECT_DATA"].format(sketch_name))
-        static = {fields["file_uid"]: file_uid,
-                  fields["sketch_name"]: sketch_name}
-        for property_ in sketch_data.findall(geo_list_xpath):
-            
+        sketch_data = tree.find(XPATHS["OBJECT_DATA"].format(sketch_name))
+        static = {FIELDS["file_uid"]: file_uid,
+                  FIELDS["sketch_name"]: sketch_name}
+        for property_ in sketch_data.findall(GEO_LIST_XPATH):
             for info in read_property_value(property_):
                 if info[Attr.TYPE] != type_:
                     continue
-                
                 geometry = property_.find(geo_type_xpath)
-                extension = geometry.find(geo_ext_xpath)
-                ignore_fields = set(geometry.attrib) | set(extension.attrib) 
+                extension = geometry.find(GEO_EXT_XPATH)
+                ignore_fields = set(geometry.attrib) | set(extension.attrib)
                 ignore_fields.remove(Attr.ID)
-                
                 for field in ignore_fields:
                     info.pop(field, None)
                 data.append({**static, **info})
@@ -242,50 +202,38 @@ def sketch_geometry_info(tree: ElementTree) -> list[dict[str, str]]:
     """Returns a tuple of field names and a tuple of values in common for each 
     sketch geometry element in the tree.
     """
-    config = _xml_config()
-    xpaths = config["xpaths"]
-    fields = config["pancad"]["fields"]
-    
-    file_uid = tree.find(xpaths["FILE_UID"]).get(Attr.VALUE)
-    sketch_xpath = xpaths["OBJECT_TYPE"].format(Sketcher.SKETCH)
-    geo_list_xpath = xpaths["PROPERTY_TYPE"].format(Part.GEOMETRY_LIST)
-    STATIC_FIELDS = (fields["file_uid"], fields["sketch_name"])
-    SHARED_FIELDS = (
-        fields["list_name"],
-        fields["list_index"],
+    file_uid = tree.find(XPATHS["FILE_UID"]).get(Attr.VALUE)
+    shared_fields = (
+        FIELDS["list_name"],
+        FIELDS["list_index"],
         Attr.ID,
         Attr.TYPE,
         Attr.MIGRATED,
         Tag.CONSTRUCTION,
         Attr.INTERNAL_GEOMETRY_TYPE,
     )
-    
     info = []
-    for sketch in tree.findall(sketch_xpath):
+    for sketch in tree.findall(SKETCH_XPATH):
         sketch_name = sketch.get(Attr.NAME)
-        sketch_data = tree.find(xpaths["OBJECT_DATA"].format(sketch_name))
-        static = {fields["file_uid"]: file_uid,
-                  fields["sketch_name"]: sketch_name}
-        for property_ in sketch_data.findall(geo_list_xpath):
+        sketch_data = tree.find(XPATHS["OBJECT_DATA"].format(sketch_name))
+        static = {FIELDS["file_uid"]: file_uid,
+                  FIELDS["sketch_name"]: sketch_name}
+        for property_ in sketch_data.findall(GEO_LIST_XPATH):
             geometry_info = read_property_value(property_)
             for geometry in geometry_info:
                 geometry = {key: geometry[key] for key in geometry
-                            if key in SHARED_FIELDS}
+                            if key in shared_fields}
                 info.append({**static, **geometry})
     return info
 
 def sketch_geometry_types(tree: ElementTree) -> list[str]:
     """Returns the types of each sketch geometry type in the file."""
-    xpaths = _xml_config()["xpaths"]
-    sketch_xpath = xpaths["OBJECT_TYPE"].format(Sketcher.SKETCH)
-    geo_list_xpath = xpaths["PROPERTY_TYPE"].format(Part.GEOMETRY_LIST)
-    
     types = set()
-    for sketch in tree.findall(sketch_xpath):
+    for sketch in tree.findall(SKETCH_XPATH):
         sketch_name = sketch.get(Attr.NAME)
-        sketch_data = tree.find(xpaths["OBJECT_DATA"].format(sketch_name))
-        for property_ in sketch_data.findall(geo_list_xpath):
-            for geometry in property_.findall(xpaths["GEOMETRY_LIST"]):
+        sketch_data = tree.find(XPATHS["OBJECT_DATA"].format(sketch_name))
+        for property_ in sketch_data.findall(GEO_LIST_XPATH):
+            for geometry in property_.findall(XPATHS["GEOMETRY_LIST"]):
                 types.add(geometry.attrib[Attr.TYPE])
     return list(types)
 
@@ -296,16 +244,8 @@ def view_provider_properties(tree: ElementTree, name: str
     :param tree: The ElementTree of a FCStd GuiDocument.xml
     :param name: The name of an object in the FCStd file
     """
-    xpaths = _xml_config()["xpaths"]
-    provider = tree.find(xpaths["VIEW_PROVIDER"].format(name))
+    provider = tree.find(XPATHS["VIEW_PROVIDER"].format(name))
     data = dict(provider.attrib)
-    for property_ in provider.findall(xpaths["PROPERTY"]):
+    for property_ in provider.findall(XPATHS["PROPERTY"]):
         data[property_.get(Attr.NAME)] = read_property_value(property_)
     return data
-
-@cache
-def _xml_config() -> dict[str, str]:
-    """Returns the xml configuration dict of the freecad.toml file"""
-    FREECAD_TOML = Path(resources.__file__).parent / "freecad.toml"
-    with open(FREECAD_TOML, "rb") as file:
-        return tomllib.load(file)["xml"]
