05b0ce13515ed47266268464adcd0dec0af08a04
unknown <georgesparks673@gmail.com>
2026-03-28 14:06:04 -0500

    linted validators, removed unnecessary elifs/elses, f strings, compressed condition checks

--- src/pancad/graphics/svg/validators.py
+++ src/pancad/graphics/svg/validators.py
@@ -4,27 +4,27 @@ browser.
 """
 
 import re
+from numbers import Real
 
 from pancad.graphics.svg import enum_color_keywords
 
-float_re = r"[+-]?[0-9]*\.[0-9]+"
-integer_re = "[+-]?[0-9]+"
-number_re = "(" + float_re + "|" + integer_re + ")"
-hex_6_color_re = "#[0-9A-Fa-f]{6}"
-hex_3_color_re = "#[0-9A-Fa-f]{3}"
-rgb_number_re = r"[rR][gG][bB]\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)"
-rgb_percent_re = r"[rR][gG][bB]\([0-9]{1,3}%,[0-9]{1,3}%,[0-9]{1,3}%\)"
-style_length_units_re = "(em|ex|px|in|cm|mm|pt|pc)"
-presentation_length_units_re = "(em|ex|px|in|cm|mm|pt|pc|%)"
-length_re = "^" + number_re + presentation_length_units_re + "?$"
+FLOAT_RE = r"[+-]?[0-9]*\.[0-9]+"
+INTEGER_RE = "[+-]?[0-9]+"
+NUMBER_RE = "(" + FLOAT_RE + "|" + INTEGER_RE + ")"
+HEX_6_COLOR_RE = "#[0-9A-Fa-f]{6}"
+HEX_3_COLOR_RE = "#[0-9A-Fa-f]{3}"
+RGB_NUMBER_RE = r"[rR][gG][bB]\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)"
+RGB_PERCENT_RE = r"[rR][gG][bB]\([0-9]{1,3}%,[0-9]{1,3}%,[0-9]{1,3}%\)"
+STYLE_LENGTH_UNITS_RE = "(em|ex|px|in|cm|mm|pt|pc)"
+PRESENTATION_LENGTH_UNITS_RE = "(em|ex|px|in|cm|mm|pt|pc|%)"
+LENGTH_RE = "^" + NUMBER_RE + PRESENTATION_LENGTH_UNITS_RE + "?$"
 
-def number(setting: str | int | float) -> str:
+def number(setting: str | Real) -> str:
     """Returns the setting after checking whether it is a number"""
     setting = str(setting)
-    if re.match("^" + number_re + "$", setting):
+    if re.match("^" + NUMBER_RE + "$", setting):
         return setting
-    else:
-        raise ValueError(setting + " does not match svg number format")
+    raise ValueError(setting + " does not match svg number format")
 
 def stroke_linecap(setting: str) -> str:
     """Returns the setting after checking that it meets the rules for
@@ -38,8 +38,7 @@ def stroke_linecap(setting: str) -> str:
     if setting not in allowable_values:
         raise ValueError(f"stroke-linecap '{setting}' is not in allowable"
                          + f" values: {allowable_values}")
-    else:
-        return setting
+    return setting
 
 def stroke_linejoin(setting: str) -> str:
     """Returns the setting after checking that it meets the rules for
@@ -53,10 +52,9 @@ def stroke_linejoin(setting: str) -> str:
     if setting not in allowable_values:
         raise ValueError(f"stroke-linejoin '{setting}' is not in allowable"
                          + f" values: {allowable_values}")
-    else:
-        return setting
+    return setting
 
-def stroke_opacity(setting: float | str) -> str:
+def stroke_opacity(setting: Real) -> str:
     """Returns the setting after checking that it meets the rules for
     a stroke-opacity svg setting. If the setting is not between 0 and
     1 or is not a float/int, it will raise a ValueError.
@@ -66,11 +64,10 @@ def stroke_opacity(setting: float | str) -> str:
     """
     if setting in ["inherit"]:
         return setting
-    elif isinstance(setting, float) or isinstance(setting, int):
+    if isinstance(setting, Real):
         # Clamps value to be 0 or 1 if outside that range
         return str(sorted((0, setting, 1))[1])
-    else:
-        raise ValueError(f"'{setting}' is not a recognized opacity format")
+    raise ValueError(f"'{setting}' is not a recognized opacity format")
 
 def color(setting: str) -> str:
     """Returns the setting as is after checking that it meets the rules
@@ -81,18 +78,18 @@ def color(setting: str) -> str:
     :param setting: the color setting in hex, rgb, or svg color keyword format
     :returns: the color setting
     """
-    if re.match(r"^" + hex_6_color_re + "$", setting):
+    if re.match(r"^" + HEX_6_COLOR_RE + "$", setting):
         return setting
-    elif re.match(r"^" + hex_3_color_re + "$", setting):
+    if re.match(r"^" + HEX_3_COLOR_RE + "$", setting):
         return setting
-    elif re.match(r"^" + rgb_number_re + "$", setting):
+    if re.match(r"^" + RGB_NUMBER_RE + "$", setting):
         rgb_vals = setting[4:-1]
         rgb_vals = rgb_vals.split(",")
         for val in rgb_vals:
             if int(val) > 255:
                 raise ValueError(f"Provided value of '{setting}' has rgb >255")
         return setting
-    elif re.match(r"^" + rgb_percent_re + "$", setting):
+    if re.match(r"^" + RGB_PERCENT_RE + "$", setting):
         rgb_vals = setting[4:-1]
         rgb_vals = rgb_vals.replace("%","")
         rgb_vals = rgb_vals.split(",")
@@ -100,10 +97,9 @@ def color(setting: str) -> str:
             if int(val) > 100:
                 raise ValueError(f"Provided value of '{setting}' has rgb >100%")
         return setting
-    elif setting.upper() in enum_color_keywords.Color.__members__:
+    if setting.upper() in enum_color_keywords.Color.__members__:
         return setting
-    else:
-        raise ValueError(f"Color '{setting}' is not in a recognized format")
+    raise ValueError(f"Color '{setting}' is not in a recognized format")
 
 def paint(setting: str) -> str:
     """Returns a paint setting as is after checking that it meets svg
@@ -113,10 +109,9 @@ def paint(setting: str) -> str:
     :param setting: the paint setting
     :returns: echos back the setting
     """
-    if setting == "none" or setting == "currentColor":
+    if setting in {"none", "currentColor"}:
         return setting
-    else:
-        return color(setting)
+    return color(setting)
 
 def percentage(setting: str) -> str:
     """Returns a percentage setting as is after checking that it meets svg
@@ -127,12 +122,11 @@ def percentage(setting: str) -> str:
     :returns: echos back the setting
     """
     setting = str(setting)
-    if re.match("^" + number_re + "%$", setting):
+    if re.match("^" + NUMBER_RE + "%$", setting):
         return setting
-    else:
-        raise ValueError(f"'{setting}' is not svg 1.1 <percentage> formatted")
+    raise ValueError(f"'{setting}' is not svg 1.1 <percentage> formatted")
 
-def stroke_width(setting: str | int | float) -> str:
+def stroke_width(setting: str | Real) -> str:
     """Returns the setting after checking that the value is greater
     than 0 and that the unit is em, ex, px, in, cm, mm, pt, or pc
 
@@ -143,8 +137,7 @@ def stroke_width(setting: str | int | float) -> str:
         raise ValueError("Provided length value '"
                          + setting
                          + "' must be greater than 0")
-    else:
-        return length(setting)
+    return length(setting)
 
 def length(setting: str) -> str:
     """Returns the setting as is or after turning it to a string after
@@ -167,29 +160,26 @@ def length(setting: str) -> str:
     :returns: echos back the setting as a string
     """
     setting = str(setting)
-    if re.match(length_re, setting):
+    if re.match(LENGTH_RE, setting):
         return setting
-    else:
-        raise ValueError(f"'{setting}' is not svg 1.1 <length> format")
+    raise ValueError(f"'{setting}' is not svg 1.1 <length> format")
 
-def length_value(setting: str) -> float | int:
+def length_value(setting: str) -> Real:
     """Returns just the number of the given length without the unit.
 
     :param setting: the setting to set an svg length setting.
     :returns: the number value of the setting as a float or int
     """
     setting = length(setting)
-    length_match = re.match(length_re, setting)
-    if length_match:
-        setting = length_match[1] # Isolate numerical value
-    else:
+    length_match = re.match(LENGTH_RE, setting)
+    if not length_match:
         raise ValueError(f"Format of '{setting}' is not recognized")
-    if re.match(r"^" + float_re + "$", setting):
+    setting = length_match[1] # Isolate numerical value
+    if re.match(r"^" + FLOAT_RE + "$", setting):
         return float(setting)
-    else:
-        return int(setting)
+    return int(setting)
 
-def length_unit(setting: str | int | float) -> str:
+def length_unit(setting: str | Real) -> str:
     """Returns just the unit of the given length without the value.
     If an int, float, or number represented as a string is given
     this returns "". If the given value is just a unit string like
@@ -202,16 +192,14 @@ def length_unit(setting: str | int | float) -> str:
     setting = str(setting)
     if setting == "":
         return ""
-    elif re.match("^" + presentation_length_units_re + "$", setting):
+    if re.match("^" + PRESENTATION_LENGTH_UNITS_RE + "$", setting):
         return setting
-    else:
-        setting = length(setting)
+    setting = length(setting)
 
-    if re.match("^" + number_re + "$", setting):
+    if re.match("^" + NUMBER_RE + "$", setting):
         return ""
-    elif re.match("^" + number_re + presentation_length_units_re + "$",
+    if re.match("^" + NUMBER_RE + PRESENTATION_LENGTH_UNITS_RE + "$",
                   setting):
-        return re.search(presentation_length_units_re, setting)[0]
-    else:
-        raise ValueError(f"'{setting}' is an unrecognized format that passed"
-                         + f" length's check")
+        return re.search(PRESENTATION_LENGTH_UNITS_RE, setting)[0]
+    msg = f"'{setting}' is an unrecognized format that passed length's check"
+    raise ValueError(msg)
