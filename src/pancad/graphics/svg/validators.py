0d54454460cd307d2d500acf733f99adb8dada72
unknown <georgesparks673@gmail.com>
2026-03-28 09:27:36 -0500

    linted enum_color_keywords and modded validators to capitalize input checking

--- src/pancad/graphics/svg/validators.py
+++ src/pancad/graphics/svg/validators.py
@@ -100,7 +100,7 @@ def color(setting: str) -> str:
             if int(val) > 100:
                 raise ValueError(f"Provided value of '{setting}' has rgb >100%")
         return setting
-    elif setting in enum_color_keywords.Color.__members__:
+    elif setting.upper() in enum_color_keywords.Color.__members__:
         return setting
     else:
         raise ValueError(f"Color '{setting}' is not in a recognized format")
