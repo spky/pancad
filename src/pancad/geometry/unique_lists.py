76d16593a2cc170c2a43d278cd043cd638aca97f
unknown <georgesparks673@gmail.com>
2026-06-13 08:25:44 -0500

    added overloads for get and set item. Broke insert down into subclasses. Starting typing unique_lists

--- src/pancad/geometry/unique_lists.py
+++ src/pancad/geometry/unique_lists.py
@@ -1,9 +1,11 @@
 """A module defining the lists of unique CAD elements used by features and geometry."""
 from __future__ import annotations
 
+from abc import ABCMeta
 from collections.abc import MutableSequence
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, overload
 
+from pancad.abstract import PancadThing
 from pancad.exceptions import (
     DupeUidError,
     HasDependentsError,
@@ -11,28 +13,29 @@ from pancad.exceptions import (
 )
 
 if TYPE_CHECKING:
-    from typing import Sequence, Optional
+    from collections.abc import Iterable
+    from typing import Sequence, Optional, Any
     from uuid import UUID
 
     from pancad.abstract import (
-        AbstractFeature, AbstractGeometry, AbstractConstraint, PancadThing
+        AbstractFeature, AbstractGeometry, AbstractConstraint, AbstractGeometrySystem
     )
     from pancad.geometry.system import SketchGeometrySystem, FeatureSystem
 
 
-class UniqueCADList(MutableSequence):
-    """A class managing a mutable list of CAD elements (geometry, constraints, 
+class UniqueCADList(MutableSequence, metaclass=ABCMeta):
+    """A class managing a mutable list of CAD elements (geometry, constraints,
     features).
 
     :param parent: The object containing this list.
     :param values: elements to initialize the list with.
-    :raises DupeUidError: When trying to add multiple of elements with 
+    :raises DupeUidError: When trying to add multiple of elements with
         the same uid to the list.
     """
     __type_name = "PancadThing"
 
     def __init__(self,
-                 parent: PancadThing,
+                 parent: AbstractGeometrySystem,
                  values: Optional[Sequence[PancadThing]]=None) -> None:
         self._parent = parent
         self._values: list[PancadThing] = []
@@ -58,23 +61,15 @@ class UniqueCADList(MutableSequence):
                 f"No {self._type_name} with uid '{uid}' found."
             ) from exc
 
-    def insert(self, index: int, value: PancadThing) -> None:
-        """Inserts the pancad element into the list.
-
-        :raises DupeUidError: When a duped uid value is added to the list.
-        """
-        self._raise_if_duped_uid(value)
-        self._values.insert(index, value)
-
     def get_contents(self) -> list[PancadThing]:
-        """Returns the full list of contents, including any items in specialized 
+        """Returns the full list of contents, including any items in specialized
         indices.
         """
         return self._values
 
     # Private Methods
     def _raise_if_duped_uid(self, value: PancadThing) -> None:
-        """Raises a DupeUidError if the geometry's uid is already in the 
+        """Raises a DupeUidError if the geometry's uid is already in the
         list. Used when trying to add geometry to the list.
         """
         if value in self:
@@ -82,7 +77,7 @@ class UniqueCADList(MutableSequence):
             raise DupeUidError(msg)
 
     def _raise_if_has_dependents(self, value: PancadThing) -> None:
-        """Raises a HasDependentsError if geometry still has 
+        """Raises a HasDependentsError if geometry still has
         constraints. Used when trying to delete geometry from list.
         """
         if dependents := self._parent.get_dependents(value):
@@ -90,14 +85,25 @@ class UniqueCADList(MutableSequence):
             raise HasDependentsError(msg)
 
     # Dunders
-    def __getitem__(self, index: int) -> PancadThing:
+    @overload
+    def __getitem__(self, index: int) -> PancadThing: ...
+    @overload
+    def __getitem__(self, index: slice[int | None, int | None, int | None]
+                    ) -> list[PancadThing]: ...
+    def __getitem__(self, index):
         return self._values[index]
 
-    def __setitem__(self, index: int, value: PancadThing) -> None:
+    @overload
+    def __setitem__(self, index: int, value: PancadThing) -> None: ...
+    # TODO: Implement Second __setitem__ overload
+    @overload
+    def __setitem__(self, index: slice[int | None, int | None, int | None],
+                    value: Iterable[PancadThing]) -> None: ...
+    def __setitem__(self, index, value):
         """Sets the index to the value.
 
         :raises DupeUidError: When a duped uid value is added to the list.
-        :raises HasDependentsError: When trying to replace an element that still 
+        :raises HasDependentsError: When trying to replace an element that still
             has dependents.
         """
         if self[index].uid != value.uid:
@@ -105,9 +111,14 @@ class UniqueCADList(MutableSequence):
         self._raise_if_has_dependents(self[index])
         self._values[index] = value
 
-    def __delitem__(self, index: int) -> None:
+    # TODO: Implement second __delitem__ overload
+    @overload
+    def __delitem__(self, index: int) -> None: ...
+    @overload
+    def __delitem__(self, index: slice[int | None, int | None, int | None]) -> None: ...
+    def __delitem__(self, index):
         """Deletes the value from the list after checking deletion validity.
-        
+
         :raises HasDependentsError: Raised if the feature still has dependents.
         """
         self._raise_if_has_dependents(self[index])
@@ -116,7 +127,9 @@ class UniqueCADList(MutableSequence):
     def __len__(self) -> int:
         return len(self._values)
 
-    def __contains__(self, value: PancadThing) -> bool:
+    def __contains__(self, value: Any) -> bool:
+        if not isinstance(value, PancadThing):
+            return False
         return any(value.uid == element.uid for element in self.get_contents())
 
     def __repr__(self) -> str:
@@ -127,8 +140,8 @@ class UniqueCADList(MutableSequence):
 
 
 class SystemFeatureList(UniqueCADList):
-    """A class managing a mutable list of CAD features inside of a 
-    FeatureSystem. The list's parent does not contribute to the lists's 
+    """A class managing a mutable list of CAD features inside of a
+    FeatureSystem. The list's parent does not contribute to the lists's
     length, but is accessible at index -1.
 
     :param parent: The system containing this list.
@@ -142,17 +155,20 @@ class SystemFeatureList(UniqueCADList):
         super().__init__(parent, values)
 
     # Public Methods
-    def index(self, value: AbstractFeature, start: int=0, stop: int=None) -> int:
+    def index(self, value: AbstractFeature, start: int=0, stop: Optional[int]=None) -> int:
         if value is self._parent.feature:
             return -1
+        if stop is None:
+            return super().index(value, start)
         return super().index(value, start, stop)
 
     def insert(self, index: int, value: AbstractFeature) -> None:
-        """Inserts the object into the list and assigns its system to the 
+        """Inserts the object into the list and assigns its system to the
         list's parent.
         """
         self._raise_if_missing_dependencies(value)
-        super().insert(index, value)
+        self._raise_if_duped_uid(value)
+        self._values.insert(index, value)
         self._assign_system(value)
 
     def get_by_name(self, name: str) -> AbstractFeature:
@@ -178,7 +194,7 @@ class SystemFeatureList(UniqueCADList):
                 if feature not in self._parent]
 
     def _raise_if_missing_dependencies(self, value: AbstractFeature):
-        """Raises a MissingCADDependencyError when not all of a constraint's 
+        """Raises a MissingCADDependencyError when not all of a constraint's
         dependencies are in the list's system.
         """
         if missing := self.missing_dependencies(value):
@@ -209,7 +225,7 @@ class SystemFeatureList(UniqueCADList):
         previous_value.system = None
 
 class FeatureConstraintList(UniqueCADList):
-    """A class managing the mutable list of constraints between features and 
+    """A class managing the mutable list of constraints between features and
     their dependencies.
     """
     __type_name = "Constraint"
@@ -223,11 +239,16 @@ class FeatureConstraintList(UniqueCADList):
 
     # Public Methods
     def insert(self, index: int, value: AbstractConstraint) -> None:
-        """Inserts the object into the list and assigns its system to the 
+        """Inserts the object into the list and assigns its system to the
         list's parent.
+
+        :raises DupeUidError: When a duped uid value is added to the list.
+        :raises MissingCADDependencyError: When not all constraint
+            dependencies are in the constraint lists's parent.
         """
         self._raise_if_missing_dependencies(value)
-        super().insert(index, value)
+        self._raise_if_duped_uid(value)
+        self._values.insert(index, value)
         self._assign_system(value)
 
     def missing_dependencies(self,
@@ -250,7 +271,7 @@ class FeatureConstraintList(UniqueCADList):
         value.feature = self._parent.feature
 
     def _raise_if_missing_dependencies(self, value: AbstractConstraint):
-        """Raises a MissingCADDependencyError when not all of a constraint's 
+        """Raises a MissingCADDependencyError when not all of a constraint's
         dependencies are in the list's system.
         """
         if missing := self.missing_dependencies(value):
@@ -259,7 +280,7 @@ class FeatureConstraintList(UniqueCADList):
 
     # Dunders
     def __setitem__(self, index: int, value: AbstractConstraint) -> None:
-        """Replaces object in list and removes the old object's system and 
+        """Replaces object in list and removes the old object's system and
         feature.
         """
         previous_value = self._values[index] # -1 is not allowed here
@@ -271,8 +292,8 @@ class FeatureConstraintList(UniqueCADList):
         previous_value.feature = None
 
 class FeatureGeometryList(UniqueCADList):
-    """A class managing the list of geometry that a feature owns. Feature 
-    geometry includes any geometry that would need to be deleted if the 
+    """A class managing the list of geometry that a feature owns. Feature
+    geometry includes any geometry that would need to be deleted if the
     feature was deleted.
     """
     __type_name = "Feature Geometry"
@@ -282,10 +303,14 @@ class FeatureGeometryList(UniqueCADList):
         super().__init__(parent, values)
 
     def insert(self, index: int, value: AbstractGeometry) -> None:
-        """Inserts the object into the list and assigns its feature to the 
+        """Inserts the object into the list and assigns its feature to the
         list's parent.
+
+        :raises DupeUidError: When a duped uid value is added to the list.
         """
-        super().insert(index, value)
+
+        self._raise_if_duped_uid(value)
+        self._values.insert(index, value)
         self._assign_feature(value)
 
     def _assign_feature(self, value: AbstractGeometry) -> None:
@@ -310,20 +335,13 @@ class FeatureGeometryList(UniqueCADList):
         # Remove the feature from exiting geometry
         previous_value.feature = None
 
-class UniqueSketchElementList(UniqueCADList):
+class UniqueSketchElementList(UniqueCADList, metaclass=ABCMeta):
     """A class managing the interfaces for CAD sketch lists."""
-    def insert(self, index: int,
-               value: AbstractConstraint | AbstractGeometry) -> None:
-        """Inserts the object into the list and assigns its system to the 
-        list's parent.
-        """
-        super().insert(index, value)
-        self._assign_system(value)
 
     # Private Methods
     def _assign_system(self,
                        value: AbstractConstraint | AbstractGeometry) -> None:
-        """Assigns the system of the sketch element to the list's parent and the 
+        """Assigns the system of the sketch element to the list's parent and the
         feature to the parent's feature.
         """
         if value.system is not None:
@@ -353,12 +371,12 @@ class UniqueSketchElementList(UniqueCADList):
 
 
 class SketchGeometryList(UniqueSketchElementList):
-    """A class managing a mutable list of geometry. The list's parent does not 
+    """A class managing a mutable list of geometry. The list's parent does not
     contribute to the lists's length, but is accessible at index -1.
 
     :param parent: The SketchGeometrySystem that contains this list.
     :param values: Geometry to initialize the list with.
-    :raises DupeUidError: When trying to add multiple geometries with the same 
+    :raises DupeUidError: When trying to add multiple geometries with the same
         uid to the list.
     """
     __type_name = "Geometry"
@@ -371,6 +389,16 @@ class SketchGeometryList(UniqueSketchElementList):
     def get_contents(self) -> list[AbstractGeometry]:
         return [self._parent] + super().get_contents()
 
+    def insert(self, index: int, value: AbstractGeometry) -> None:
+        """Inserts the object into the list and assigns its system to the
+        list's parent.
+
+        :raises DupeUidError: When a duped uid value is added to the list.
+        """
+        self._raise_if_duped_uid(value)
+        self._values.insert(index, value)
+        self._assign_system(value)
+
     def __getitem__(self, index: int) -> AbstractGeometry:
         if index == -1:
             return self._parent
@@ -378,12 +406,12 @@ class SketchGeometryList(UniqueSketchElementList):
 
 
 class SketchConstraintList(UniqueSketchElementList):
-    """A class managing a mutable list of sketch constraints and their 
+    """A class managing a mutable list of sketch constraints and their
     dependencies.
 
     :param parent: The SketchGeometrySystem that contains this list.
     :param values: Constraints to initialize the list with.
-    :raises DupeUidError: When trying to add multiple constraints with the same 
+    :raises DupeUidError: When trying to add multiple constraints with the same
         uid to the list.
     """
     __type_name = "Constraint"
@@ -398,12 +426,15 @@ class SketchConstraintList(UniqueSketchElementList):
     # Public Methods
     def insert(self, index: int, value: AbstractConstraint) -> None:
         """Inserts the constraint into the constraint list.
-        
+
         :raises MissingCADDependencyError: When not all constraint
             dependencies are in the constraint lists's associated system.
+        :raises DupeUidError: When a duped uid value is added to the list.
         """
         self._raise_if_missing_dependencies(value)
-        super().insert(index, value)
+        self._raise_if_duped_uid(value)
+        self._values.insert(index, value)
+        self._assign_system(value)
 
     def missing_dependencies(self, value: AbstractConstraint
                              ) -> list[AbstractGeometry]:
@@ -413,7 +444,7 @@ class SketchConstraintList(UniqueSketchElementList):
 
     # Private Methods
     def _raise_if_missing_dependencies(self, value: AbstractConstraint):
-        """Raises a MissingCADDependencyError when not all of a constraint's 
+        """Raises a MissingCADDependencyError when not all of a constraint's
         dependencies are in the list's system.
         """
         if missing := self.missing_dependencies(value):
@@ -422,8 +453,8 @@ class SketchConstraintList(UniqueSketchElementList):
     # Dunders
     def __setitem__(self, index: int, value: AbstractConstraint) -> None:
         """Sets the index to the constraint.
-        
-        :raises MissingCADDependencyError: When not all constraint 
+
+        :raises MissingCADDependencyError: When not all constraint
             dependencies are in the constraint lists's parent.
         """
         self._raise_if_missing_dependencies(value)
