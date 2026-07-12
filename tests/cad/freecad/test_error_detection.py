4a2163f15bc243b374aa4d2f460422674aade159
unknown <georgesparks673@gmail.com>
2026-07-12 10:22:08 -0500

    chore: moved invalid pads/sketches to freecad data folder and remove all references to sameple_freecad directory

--- tests/cad/freecad/test_error_detection.py
+++ tests/cad/freecad/test_error_detection.py
@@ -1,50 +1,44 @@
+"""A collection of tests for pancad's ability to check FreeCAD files for validity and errors."""
 from pathlib import Path
-import unittest
 
-from pancad.cad.freecad.freecad_python import (
-    call_freecad_python, validate_freecad
-)
+import pytest
+
+from pancad.cad.freecad.freecad_python import call_freecad_python, validate_freecad
 from pancad.cad.freecad import error_detection
-from tests import sample_freecad
 
-class TestErrorDetection(unittest.TestCase):
-    
-    def setUp(self):
-        self.sample_dir = Path(sample_freecad.__file__).parent
-    
-    def test_invalid_sketches(self):
+class TestErrorDetection:
+    """Tests for detecting invalid FreeCAD files using FreeCAD's application."""
+
+    def test_invalid_sketches(self, shared_datadir: Path):
+        """Test invalid sketch detection: detached sketches, unconstrained sketches, and errored
+        sketches.
+        """
         filename = "invalid_sketches.FCStd"
-        test = call_freecad_python(error_detection.__file__,
-                                   self.sample_dir / filename)
-        
+        test = call_freecad_python(error_detection.__file__, shared_datadir / filename)
+
         DETACHED = "unattached"
         NO_UNCONSTRAINED = 5
         NO_ERRORS = 4
-        
-        with self.subTest(detached=DETACHED):
-            detached_errs = test["detached"]
-            self.assertEqual(detached_errs[0].split(":")[-1].strip(), DETACHED)
-            self.assertEqual(len(detached_errs), 1)
-        
-        with self.subTest(expected_unconstrained=NO_UNCONSTRAINED):
-            self.assertEqual(len(test["unconstrained"]), NO_UNCONSTRAINED)
-        
-        with self.subTest(expected_errors=NO_ERRORS):
-            self.assertEqual(len(test["error"]), NO_ERRORS)
-    
-    def test_invalid_sketches_validation(self):
-        with self.assertRaises(ValueError):
-            validate_freecad(self.sample_dir / "invalid_sketches.FCStd")
-    
-    def test_invalid_pads(self):
+
+        detached_errs = test["detached"]
+        assert detached_errs[0].split(":")[-1].strip() == DETACHED
+        assert len(detached_errs) == 1
+        assert len(test["unconstrained"]) == NO_UNCONSTRAINED
+        assert len(test["error"]) == NO_ERRORS
+
+    def test_invalid_sketches_validation(self, shared_datadir: Path):
+        """Test that validate_freecad can raise an error on incorrect files."""
+        with pytest.raises(ValueError):
+            validate_freecad(shared_datadir / "invalid_sketches.FCStd")
+
+    def test_invalid_pads(self, shared_datadir: Path):
+        """Test that invalid pads in freecad can be detected."""
         filename = "invalid_pads.FCStd"
-        test = call_freecad_python(error_detection.__file__,
-                                   self.sample_dir / filename)
-        with self.subTest(expected_unconstrained=1):
-            self.assertEqual(len(test["unconstrained"]), 1)
-        with self.subTest(expected_errors=1):
-            self.assertEqual(len(test["error"]), 1)
-    
-    def test_invalid_pads_validation(self):
-        with self.assertRaises(ValueError):
-            validate_freecad(self.sample_dir / "invalid_pads.FCStd")
+        test = call_freecad_python(error_detection.__file__, shared_datadir / filename)
+        assert len(test["unconstrained"]) == 1
+        assert len(test["error"]) == 1
+
+    def test_invalid_pads_validation(self, shared_datadir: Path):
+        """Test that invalid pads in freecad raise an error when validated."""
+        with pytest.raises(ValueError):
+            validate_freecad(shared_datadir / "invalid_pads.FCStd")
