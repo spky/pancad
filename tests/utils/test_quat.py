cf94f6c04fccc0fff45d8f2e93f7d91f4a6b9513
unknown <georgesparks673@gmail.com>
2026-08-05 08:28:08 -0500

    test: Added tests for arithmetic dunders and rotation of a couple vectors

--- tests/utils/test_quat.py
+++ tests/utils/test_quat.py
@@ -9,7 +9,25 @@ import numpy as np
 from pancad.utils.quat import Quat
 
 if TYPE_CHECKING:
+    from tests._typing import GeometrySampleData, ChangeTest
+
     QuatCoefficients = tuple[float, float, float, float]
+
+@pytest.fixture(name="quat_sample")
+def fixture_quat_sample(data_geo_util_sample_quats: GeometrySampleData) -> Quat:
+    """A quaternion generated from geometry util sample data."""
+    vectors, scalars = (data_geo_util_sample_quats["vectors"],
+                        data_geo_util_sample_quats["scalars"])
+    if "angle" in scalars:
+        return Quat.from_angle(scalars["angle"], vectors["axis"])
+    raise RuntimeError(f"Missing angle from scalars: {scalars}")
+
+@pytest.fixture(name="quat_expected")
+def fixture_quat_expected(data_geo_util_sample_quats: GeometrySampleData) -> QuatCoefficients:
+    """The expected vectorized quaternion generated from geometry util sample data."""
+    vectors, scalars = (data_geo_util_sample_quats["vectors"],
+                        data_geo_util_sample_quats["scalars"])
+    return (scalars["scalar"], *vectors["vector"])
 
 @pytest.mark.parametrize("coefficients", [(1, 2, 3, 4)])
 class TestInitializationTypes:
@@ -52,6 +70,18 @@ class TestSpotChecks:
         """Test the conjugate returns the Quat with negative vector components."""
         assert quat.conjugate[1:] == tuple(-c for c in quat.vector)
 
+    def test_inverse(self, quat: Quat) -> None:
+        """Test the inverse returns the normalized quaternion conjugate."""
+        assert tuple(quat.inverse) == tuple(quat.conjugate / np.linalg.norm(quat))
+
+    def test_invert(self, quat: Quat) -> None:
+        """Test that the invert dunder of the quaternion returns its inverse."""
+        assert tuple(~quat) == tuple(quat.inverse)
+
+    def test_negation(self, quat: Quat) -> None:
+        """Test the neg dunder returns the quaternion multiplied by negative 1."""
+        assert tuple(-quat) == tuple(-np.array(quat))
+
     def test_quat_equal(self, quat: Quat) -> None:
         """Test Quat can be equal to a new Quat and is not equal to its conjugate."""
         assert quat == Quat(quat)
@@ -61,3 +91,50 @@ class TestSpotChecks:
         """Test Quat's repr dunder labels the scalar and vector components."""
         w, x, y, z = quat
         assert repr(quat) == f"[{w}, {x}i, {y}j, {z}k]"
+
+    def test_scalar_division(self, quat: Quat) -> None:
+        """Test truediv dunder for scalar division."""
+        np.testing.assert_array_equal(np.array(quat / 4), np.array(quat) / 4)
+
+    def test_scalar_multiplication(self, quat: Quat) -> None:
+        """Test truediv dunder for scalar division."""
+        np.testing.assert_array_equal(np.array(quat * 4), np.array(quat) * 4)
+
+    def test_quat_addition(self, quat: Quat) -> None:
+        """Test the add dunder for quaternion addition."""
+        np.testing.assert_array_equal(np.array(quat + quat), np.array(quat) + np.array(quat))
+
+    def test_quat_subtraction(self, quat: Quat) -> None:
+        """Test the sub dunder for quaternion subtraction."""
+        np.testing.assert_array_equal(np.array(quat - quat), np.array(quat) - np.array(quat))
+
+    def test_len(self, quat: Quat) -> None:
+        """Test the length of the quaternion is always 4."""
+        assert len(quat) == 4
+
+class TestSamples:
+    """Test initializing sample quaternions."""
+
+    def test_vector(self, quat_sample: Quat, quat_expected: QuatCoefficients) -> None:
+        """Test the sample matches the expected vector."""
+        assert quat_sample.vector == pytest.approx(quat_expected[1:])
+
+    def test_scalar(self, quat_sample: Quat, quat_expected: QuatCoefficients) -> None:
+        """Test the sample matches the expected scalar."""
+        assert quat_sample.scalar == pytest.approx(quat_expected[0])
+
+    def test_coefficients(self, quat_sample: Quat, quat_expected: QuatCoefficients) -> None:
+        """Test that each of the sample's single coefficient getters match the expected value."""
+        w, x, y, z = quat_expected
+        pairs = [(quat_sample.scalar, w), (quat_sample.w, w),
+                 (quat_sample.x, x), (quat_sample.y, y), (quat_sample.z, z)]
+        for quat_value, expected_value in pairs:
+            assert quat_value == pytest.approx(expected_value)
+
+class TestChanges:
+    """Test changing vectors with quaternions."""
+
+    def test_rotate(self, changes_quat_rotate: ChangeTest) -> None:
+        sample, change = changes_quat_rotate
+        quat = Quat.from_angle(change["scalars"]["angle"], change["vectors"]["axis"])
+        assert quat.rotate(sample["vectors"]["direction"]) == change["vectors"]["new"]
