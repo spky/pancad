fix: Replaced all quaternion module calls with the new Quat class and ran all tests

src/pancad/cad/freecad/xml_utils.py
@@ -15,14 +15,13 @@ from pancad.cad.freecad.constants import (ConstraintSubPart as CSP,
                                           ConstraintType,
                                           InternalGeometryType)
 from pancad.utils.trigonometry import is_clockwise
+from pancad.utils.quat import Quat
 
 if TYPE_CHECKING:
     from typing import NamedTuple
     from collections.abc import Callable
     from typing import Any, NoReturn
     from xml.etree.ElementTree import Element, ElementTree
-
-    import quaternion
 
     from pancad.cad.freecad.read_xml import FreeCADGeometryXML, FreeCADConstraintXML
 
@@ -464,7 +463,7 @@ class FreeCADPlacement:
     """Dataclass tracking a FreeCAD Placement object's properties from FCStd xml
     """
     location: tuple[float, float, float]
-    quat: quaternion.quaternion
+    quat: Quat
     o_vector: tuple[float, float, float]
 
     @classmethod
@@ -474,7 +473,7 @@ class FreeCADPlacement:
         location = read_vector(element, xyz, "P", False)
         o_vector = read_vector(element, xyz, "O", False)
         quat_vector = read_vector(element, ["Q0", "Q1", "Q2", "Q3"], is_2d=False)
-        return cls(location, np.quaternion(*quat_vector), o_vector)
+        return cls(location, Quat(*quat_vector), o_vector)
 
 @dataclasses.dataclass
 class PropertyPartShape:

src/pancad/geometry/coordinate_system.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Self
 
 import numpy as np
-import quaternion
 
 from pancad.abstract import AbstractGeometry
 from pancad.constants import ConstraintReference as CR
@@ -17,6 +16,7 @@ from pancad.utils.trigonometry import yaw_pitch_roll
 from pancad.utils.geometry import get_rotation_quat
 from pancad.utils.pancad_types import VectorLike
 from pancad.utils.text_formatting import format_vector
+from pancad.utils.quat import Quat
 
 if TYPE_CHECKING:
     from typing import NoReturn
@@ -37,7 +37,7 @@ class CoordinateSystem(AbstractGeometry):
 
     def __init__(self,
                  origin: Point | SpaceVector,
-                 rotation: np.ndarray | np.quaternion=None,
+                 rotation: np.ndarray | Quat=None,
                  *, uid: str=None) -> None:
         self.uid = uid
         if not isinstance(origin, Point):
@@ -121,7 +121,7 @@ class CoordinateSystem(AbstractGeometry):
             comparisons.append(geometry.is_equal(other.get_reference(ref)))
         return all(comparisons)
 
-    def get_quaternion(self) -> np.quaternion:
+    def get_quaternion(self) -> Quat:
         """Returns a quaternion that can be used to rotate other vectors from
         the canonical cartesian coordinate system (1, 0, 0), (0, 1, 0),
         (0, 0, 1) to this coordinate system.
@@ -134,13 +134,11 @@ class CoordinateSystem(AbstractGeometry):
         for ref in (CR.X, CR.Y, CR.Z):
             canon_axis = canon_cs.get_reference(ref)
             cs_axis = self.get_reference(ref)
-            quats[ref] = get_rotation_quat(canon_axis.direction,
-                                           cs_axis.direction)
-        quats = {ref: q for ref, q in quats.items()
-                 if not np.isclose(q, np.quaternion(1, 0, 0, 0))}
+            quats[ref] = get_rotation_quat(canon_axis.direction, cs_axis.direction)
+        quats = {ref: q for ref, q in quats.items() if not np.allclose(q, Quat(1, 0, 0, 0))}
         if not quats:
             # All quaternions were identity quaternions, so just return one.
-            return np.quaternion(1, 0, 0, 0)
+            return Quat(1, 0, 0, 0)
         ref = next(iter(quats)) # Get one of the remaining references
         if np.allclose([q for _, q in quats.items()], [quats[ref]] * len(quats)):
             # Since identity quats have been filtered out, if all the leftover
@@ -154,14 +152,14 @@ class CoordinateSystem(AbstractGeometry):
                 # The the remaining axes are flipped, but have rotations that are
                 # in opposite directions. Either will work.
                 return quats[ref_1]
-            rot_axis_1 = quaternion.as_vector_part(quats[ref_1])
-            rot_axis_2 = quaternion.as_vector_part(quats[ref_2])
+            rot_axis_1 = quats[ref_1].vector
+            rot_axis_2 = quats[ref_2].vector
             if np.dot(rot_axis_1, rot_axis_2) == 0:
                 # Both remaining axes must have been flipped around if the
                 # rotation axes are perpendicular.
                 shared_axis = np.cross(canon_cs.get_reference(ref_1).direction,
                                        canon_cs.get_reference(ref_2).direction)
-                return np.quaternion(0, *shared_axis)
+                return Quat(0, *shared_axis)
         msg = ("Failed to find quaternion to rotate to this CoordinateSystem's"
                f" axes: {self.x_axis}, {self.y_axis}, {self.z_axis}."
                f" Leftover Quaternion Candidates: {quats}")
@@ -180,7 +178,7 @@ class CoordinateSystem(AbstractGeometry):
             geometry.update(other.get_reference(ref))
         return self
 
-    def rotate(self, rotation: np.ndarray | np.quaternion) -> Self:
+    def rotate(self, rotation: np.ndarray | Quat) -> Self:
         """Rotates the system with a rotation matrix or quaternion."""
         for ref, geometry in self.children.items():
             if ref in (CR.ORIGIN, CR.CORE):
@@ -289,7 +287,7 @@ class Pose(AbstractGeometry):
         """Moves the Pose to a new location with no rotation."""
         self.coordinate_system.move_to_point(location)
 
-    def rotate(self, rotation: np.ndarray | quaternion.quaternion) -> Self:
+    def rotate(self, rotation: np.ndarray | Quat) -> Self:
         """Rotates the pose with a rotation matrix or quaternion."""
         self.coordinate_system.rotate(rotation)
         return self

src/pancad/utils/geometry.py
@@ -9,9 +9,9 @@ from numbers import Real
 from warnings import catch_warnings
 
 import numpy as np
-import quaternion
 
 from pancad.utils import trigonometry as trig
+from pancad.utils.quat import Quat
 
 if TYPE_CHECKING:
     from typing import Any
@@ -239,8 +239,7 @@ def get_perpendicular(vector: Space3DVector) -> Space3DVector:
     ortho = np.cross(vector, ortho_map[x < y, x < z, y < z])
     return trig.get_unit_vector(ortho)
 
-def get_rotation_quat(start: Space3DVector, target: Space3DVector
-                      ) -> np.quaternion:
+def get_rotation_quat(start: Space3DVector, target: Space3DVector) -> Quat:
     """Returns a (non-unique) shortest-arc quaternion to rotate the start vector
     to the target vector.
 
@@ -263,14 +262,14 @@ def get_rotation_quat(start: Space3DVector, target: Space3DVector
     if any(np.allclose(vector, (0, 0, 0)) for vector in [start, target]):
         msg = f"start/target cannot be zero vector: {start}, {target}"
         raise ValueError(msg)
-    quat = np.quaternion(scalar, *axis)
-    norm = np.linalg.norm(quaternion.as_float_array(quat))
+    quat = Quat(scalar, *axis)
+    norm = np.linalg.norm(quat)
     if np.isclose(norm, 0):
         # If the norm of the quaternion is 0, the vectors are anti-parallel.
         # Anti-parallel vectors have an infinite number of shortest arc
         # quaternions, so an arbitrary perpendicular vector must be used as a
         # rotation axis.
         axis = get_perpendicular(start)
-        quat = np.quaternion(scalar, *axis)
-        norm = np.linalg.norm(quaternion.as_float_array(quat))
+        quat = Quat(scalar, *axis)
+        norm = np.linalg.norm(quat)
     return quat / norm

tests/geometry/test_coordinate_system.py
@@ -8,10 +8,10 @@ from pprint import pp
 
 import pytest
 import numpy as np
-import quaternion # pylint: disable=unused-import
 
 from pancad.constants import ConstraintReference as CR
 from pancad.geometry.coordinate_system import CoordinateSystem
+from pancad.utils.quat import Quat
 from pancad.utils.trigonometry import rotation_2, to_1d_tuple
 
 if TYPE_CHECKING:
@@ -120,7 +120,7 @@ def _rotate_3d_quaternion_params(origin: Space3DVector):
         quat_w = cos(radians(angle / 2))
         quat_ijk = map(lambda a, b: a * sin(radians(b) / 2),
                        rotation_axis, repeat(angle))
-        quat = np.quaternion(quat_w, *quat_ijk)
+        quat = Quat(quat_w, *quat_ijk)
         x_vec, y_vec, z_vec = INIT_ROTATION_AXIS_RESULTS[id_]
         expected = {
             CR.ORIGIN: origin,