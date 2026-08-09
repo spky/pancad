7cc183973d9fb9d2a03874764f1f0e488349b952
unknown <georgesparks673@gmail.com>
2026-08-09 12:16:17 -0500

    test: Type safed the remaining previously existing coordinate system tests and added system to lint/type checking

--- tests/geometry/test_coordinate_system.py
+++ tests/geometry/test_coordinate_system.py
@@ -1,24 +1,21 @@
 """Tests for pancad's CoordinateSystem geometry class."""
 from __future__ import annotations
 
-from itertools import repeat
-from math import radians, sqrt, cos, sin
 from typing import TYPE_CHECKING
-from pprint import pp
 
 import pytest
-import numpy as np
 
 from pancad.constants import ConstraintReference as CR
 from pancad.geometry.coordinate_system import CoordinateSystem
-from pancad.utils.quat import Quat
-from pancad.utils.trigonometry import rotation_2, to_1d_tuple
+from pancad.utils.trigonometry import rotation_2
 
 if TYPE_CHECKING:
-    from numbers import Real
-    from pancad.utils.pancad_types import SpaceVector, Space3DVector, Space2DVector, Numpy2D
+    from tests._typing import ChangeTest
 
-    from tests._typing import GeometrySampleData, ChangeTest
+@pytest.fixture(name="canon_3d_system")
+def fixture_canon_3d_system() -> CoordinateSystem:
+    """An unrotated 3D CoordinateSystem centered at the origin."""
+    return CoordinateSystem((0, 0, 0))
 
 class TestRotations:
     """Tests for verifying CoordinateSystem element can rotate using matrices and quaternions."""
@@ -51,38 +48,34 @@ class TestRotations:
         start = CoordinateSystem(sample["vectors"]["origin"])
         assert start.rotate(target.get_quaternion()).is_equal(target)
 
-@pytest.fixture(name="canon_3d_system")
-def fixture_canon_3d_system():
-    """An unrotated 3D CoordinateSystem centered at the origin."""
-    return CoordinateSystem((0, 0, 0))
+class TestSpotChecks:
+    """Tests for confirming basic functionality of 3D CoordinateSystem elements haven't broken."""
 
-def test_is_equal_3d(canon_3d_system):
-    """Test whether coordinate_systems can compare each other's equality."""
-    assert canon_3d_system.is_equal(canon_3d_system)
+    def test_3d_is_equal(self, canon_3d_system: CoordinateSystem) -> None:
+        """Test whether coordinate_systems can compare each other's equality."""
+        assert canon_3d_system.is_equal(canon_3d_system)
 
-def test_2d_repr_dunder():
-    """Test that the CoordinateSystem repr runs and has info for 2D systems."""
-    assert repr(CoordinateSystem((0, 0))) == "<CoordinateSystem(0,0)X(1,0)Y(0,1)>"
+    def test_3d_repr_dunder(self, canon_3d_system: CoordinateSystem) -> None:
+        """Test that the CoordinateSystem repr runs and has info for 3D systems."""
+        assert repr(canon_3d_system) == "<CoordinateSystem(0,0,0)X(1,0,0)Y(0,1,0)Z(0,0,1)>"
 
-def test_3d_repr_dunder(canon_3d_system):
-    """Test that the CoordinateSystem repr runs and has info for 3D systems."""
-    assert repr(canon_3d_system) == "<CoordinateSystem(0,0,0)X(1,0,0)Y(0,1,0)Z(0,0,1)>"
+    def test_3d_move_to_point(self, canon_3d_system: CoordinateSystem) -> None:
+        """Test 3D CoordinateSystems can be move to other points."""
+        canon_3d_system.move_to_point((1, 1, 1))
+        assert canon_3d_system.origin.cartesian == pytest.approx((1,1,1))
+        axes = {CR.X: (0, 1, 1), CR.Y: (1, 0, 1), CR.Z: (1, 1, 0)}
+        for ref, vec in axes.items():
+            assert canon_3d_system.axes[ref].reference_point.cartesian == pytest.approx(vec)
+        planes = {CR.XY: (0, 0, 1), CR.XZ: (0, 1, 0), CR.YZ: (1, 0, 0)}
+        for ref, vec in planes.items():
+            assert canon_3d_system.planes[ref].reference_point.cartesian == pytest.approx(vec)
 
-def test_3d_move_to_point(canon_3d_system):
-    """Test 3d CoordinateSystems can be move to other points."""
-    canon_3d_system.move_to_point((1, 1, 1))
-    assert canon_3d_system.origin.cartesian == pytest.approx((1,1,1))
-    axes = {CR.X: (0, 1, 1), CR.Y: (1, 0, 1), CR.Z: (1, 1, 0)}
-    for ref, vec in axes.items():
-        axis = canon_3d_system.get_reference(ref)
-        assert axis.reference_point.cartesian == pytest.approx(vec)
-    planes = {CR.XY: (0, 0, 1), CR.XZ: (0, 1, 0), CR.YZ: (1, 0, 0)}
-    for ref, vec in planes.items():
-        plane = canon_3d_system.get_reference(ref)
-        assert plane.reference_point.cartesian == pytest.approx(vec)
+    def test_3d_update(self, canon_3d_system: CoordinateSystem) -> None:
+        """Test that coordinate systems can be updated to other coordinate systems"""
+        new = CoordinateSystem((2,2,2))
+        canon_3d_system.update(new)
+        assert canon_3d_system.origin.cartesian == pytest.approx(new.origin.cartesian)
 
-def test_update(canon_3d_system):
-    """Test that coordinate systems can be updated to other coordinate systems"""
-    new = CoordinateSystem((2,2,2))
-    canon_3d_system.update(new)
-    assert canon_3d_system.origin.cartesian == pytest.approx(new.origin.cartesian)
+    def test_2d_repr_dunder(self) -> None:
+        """Test that the CoordinateSystem repr runs and has info for 2D systems."""
+        assert repr(CoordinateSystem((0, 0))) == "<CoordinateSystem(0,0)X(1,0)Y(0,1)>"
